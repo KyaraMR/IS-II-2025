64b03316fadfbc3eae8aa56421f2a1d7633dc751
KyaraMR <kyara.munoz@ucr.ac.cr>
2025-08-21 08:03:11 -0600

    Commit inicial de proyecto ASP.NET Core MVC

---
+++ Laboratorio1_C15380/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Laboratorio1_C15380.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
