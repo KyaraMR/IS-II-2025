838653a39ee4eec66528fbe64014426fd47112e6
KyaraMR <kyara.munoz@ucr.ac.cr>
2025-11-14 23:58:53 -0600

    lab 7

---
+++ lab6/UIAutomationTests/UIAutomationTests/Selenium.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UIAutomationTests
+{
+    public class Selenium
+    {
+        IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Enter_To_List_Of_Countries_Test()
+        {
+            // Arrange
+            // Abre una nueva ventana
+            var URL = "http://localhost:8081/";
+            // Maximiza la pantalla
+            _driver.Manage().Window.Maximize();
+
+            // Act
+            // Navega a la página que se necesita probar
+            _driver.Navigate().GoToUrl(URL);
+
+            // Assert
+            // No es un buen ejemplo de assert, use uno diferente
+            Assert.IsNotNull(_driver);
+        }
+    }
+}
